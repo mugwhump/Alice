fa48972a9a05511ce8e1c74fd9818005c8bfe0ac
Grayson Bartlet <grayson.bartlet@shaw.ca>
2015-04-16 11:33:05 -0700

    Room exit animations, screen fades out, changed some textures

--- Assets/Scripts/DoneSceneFadeInOut.cs
+++ Assets/Scripts/DoneSceneFadeInOut.cs
@@ -3,74 +3,89 @@ using System.Collections;
 
 public class DoneSceneFadeInOut : MonoBehaviour
 {
-	public float fadeSpeed = 1.5f;			// Speed that the screen fades to and from black.
+	public float fadetime;			// Speed that the screen fades to and from black.
 	
 	
-	private bool sceneStarting = true;		// Whether or not the scene is still fading in.
+//	private bool sceneStarting = true;		// Whether or not the scene is still fading in.
+	private bool sceneEnding = false;		// Whether or not the scene is still fading out.
+	public float alphaFadeValue;
+	public Texture blackTexture;
 	
 	
-	void Awake ()
-	{
-		// Set the texture so that it is the the size of the screen and covers it.
-		GetComponent<GUITexture>().pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
-	}
+//	void Awake ()
+//	{
+//		// Set the texture so that it is the the size of the screen and covers it.
+//		GetComponent<GUITexture>().pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+//	}
 	
 	
 	void Update ()
 	{
 		// If the scene is starting...
-		if(sceneStarting)
-			// ... call the StartScene function.
-			StartScene();
+//		if(sceneStarting)
+//			// ... call the StartScene function.
+//			StartScene();
+		if (sceneEnding)
+			EndScene ();
 	}
-	
-	
-	void FadeToClear ()
-	{
-		// Lerp the colour of the texture between itself and transparent.
-		GetComponent<GUITexture>().color = Color.Lerp(GetComponent<GUITexture>().color, Color.clear, fadeSpeed * Time.deltaTime);
-	}
-	
-	
-	void FadeToBlack ()
-	{
-		// Lerp the colour of the texture between itself and black.
-		GetComponent<GUITexture>().color = Color.Lerp(GetComponent<GUITexture>().color, Color.black, fadeSpeed * Time.deltaTime);
-	}
-	
-	
-	void StartScene ()
-	{
-		// Fade the texture to clear.
-		FadeToClear();
-		
-		// If the texture is almost clear...
-		if(GetComponent<GUITexture>().color.a <= 0.05f)
-		{
-			// ... set the colour to clear and disable the GUITexture.
-			GetComponent<GUITexture>().color = Color.clear;
-			GetComponent<GUITexture>().enabled = false;
-			
-			// The scene is no longer starting.
-			sceneStarting = false;
+
+	void OnGUI(){
+		if (sceneEnding) {
+			alphaFadeValue = Mathf.Clamp01 (alphaFadeValue + (Time.deltaTime / fadetime));
+			GUI.color = new Color (alphaFadeValue, alphaFadeValue, alphaFadeValue, alphaFadeValue);
+			Rect hurr = new Rect (0, 0, Screen.width, Screen.height);
+			GUI.DrawTexture (hurr, blackTexture);
 		}
 	}
 
-	void onTriggerEnter(Collider other) {
-		EndScene ();
+	
+	
+//	void FadeToClear ()
+//	{
+//		// Lerp the colour of the texture between itself and transparent.
+//		GetComponent<GUITexture>().color = Color.Lerp(GetComponent<GUITexture>().color, Color.clear, fadeSpeed * Time.deltaTime);
+//	}
+//	
+//	
+//	void FadeToBlack ()
+//	{
+//		// Lerp the colour of the texture between itself and black.
+//		//GetComponent<GUITexture>().color = Color.Lerp(GetComponent<GUITexture>().color, Color.black, fadeSpeed * Time.deltaTime);
+//	}
+	
+	
+//	void StartScene ()
+//	{
+//		// Fade the texture to clear.
+//		FadeToClear();
+//		
+//		// If the texture is almost clear...
+//		if(GetComponent<GUITexture>().color.a <= 0.05f)
+//		{
+//			// ... set the colour to clear and disable the GUITexture.
+//			GetComponent<GUITexture>().color = Color.clear;
+//			GetComponent<GUITexture>().enabled = false;
+//			
+//			// The scene is no longer starting.
+//			sceneStarting = false;
+//		}
+//	}
+
+	void OnTriggerEnter(Collider other) {
+		sceneEnding = true;
 	}
 	
-	public void EndScene ()
-	{
-		// Make sure the texture is enabled.
-		GetComponent<GUITexture>().enabled = true;
-		
-		// Start fading towards black.
-		FadeToBlack();
-		
-		// If the screen is almost black...
-		if(GetComponent<GUITexture>().color.a >= 0.95f)
-			// ... reload the level.
-			Application.LoadLevel(0);
-	}
+//	public void EndScene ()
+//	{
+//		// Make sure the texture is enabled.
+//		GetComponent<GUITexture>().enabled = true;
+//		
+//		// Start fading towards black.
+//		FadeToBlack();
+//		
+//		// If the screen is almost black...
+////		if(GetComponent<GUITexture>().color.a >= 0.95f)
+////			// ... reload the level.
+////			Application.LoadLevel(0);
+//	}
 }
